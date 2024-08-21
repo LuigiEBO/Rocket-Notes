39fe5d24e7f686362464c942b312f1d078339f2f
luigi <luigigames2903@gmail.com>
2024-08-21 18:52:43 -0300

    Children

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,20 @@
 
-import {Container} from "./style"
+import {Container, Links} from "./style"
 import { Header } from "../../components/header"
 import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
 export function Details() {
   return (
     <Container>
       <Header/>
+      <Section title="Links Úteis">
+        <Links>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+        </Links>
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   )
