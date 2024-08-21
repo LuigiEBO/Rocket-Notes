ba308fab53bd4930487c0773f1644d5c36721bb5
luigi <luigigames2903@gmail.com>
2024-08-21 19:00:11 -0300

    Marcadores

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,16 +3,22 @@ import {Container, Links} from "./style"
 import { Header } from "../../components/header"
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tags"
 export function Details() {
   return (
     <Container>
-      <Header/>
+      <Header />
       <Section title="Links Úteis">
         <Links>
           <li>
             <a href="#">Link 1</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="Express" />
+        <Tag title="React" />
       </Section>
 
       <Button title="Voltar" />
