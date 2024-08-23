621f7eb6d726d2a33822fab66c7dcc0b9f8188dd
luigi <luigigames2903@gmail.com>
2024-08-22 21:29:16 -0300

    finalizando a interface de detalhes

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,5 @@
 
-import {Container, Links} from "./style"
+import {Container, Links, Content} from "./style"
 import { Header } from "../../components/header"
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
@@ -9,21 +9,33 @@ export function Details() {
   return (
     <Container>
       <Header />
-      <ButtonText title="Excluir Nota"/>
-      <Section title="Links Úteis">
-        <Links>
-          <li>
-            <a href="#">Link 1</a>
-          </li>
-        </Links>
-      </Section>
 
-      <Section title="Marcadores">
-        <Tag title="Express" />
-        <Tag title="React" />
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota" />
 
-      <Button title="Voltar" />
+            <h1>Introdução ao React</h1>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptas est aliquam ut consequuntur rem officiis enim aut quae tempora, qui, veniam obcaecati dolores consequatur adipisci explicabo nemo consectetur recusandae.
+            </p>
+
+          <Section title="Links Úteis">
+            <Links>
+              <li>
+                <a href="#">Link 1</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="Express" />
+            <Tag title="React" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
