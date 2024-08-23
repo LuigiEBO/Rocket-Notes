75b0dbd6c9adbab071f204d1d3f54b534cec9ebf
luigi <luigigames2903@gmail.com>
2024-08-22 21:15:53 -0300

    ButtonText

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,11 +3,13 @@ import {Container, Links} from "./style"
 import { Header } from "../../components/header"
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
+import { ButtonText } from "../../components/ButtonText"
 import { Tag } from "../../components/Tags"
 export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Excluir Nota"/>
       <Section title="Links Úteis">
         <Links>
           <li>
