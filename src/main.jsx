63d4d1971877160b47965fe0a9a29bf758933eca
luigi <luigigames2903@gmail.com>
2024-08-29 21:55:05 -0300

    Profile

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from "styled-components"
 import theme from "./styles/theme"
 import GlobalStyles from './styles/global'
-import {SignUp} from './pages/SignUp'
+import {Profile} from './pages/Profile'
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </StrictMode>
 )
